e84bb37f8742cec4c35827ad61c38517953ac2cc
01fe22bca093@kletech.ac.in <01fe22bca093@kletech.ac.in>
2024-03-31 00:33:57 +0530

    code for student

---
+++ Student.java
@@ -0,0 +1,42 @@
+public class Student{
+
+    String name="Ranjita.H.Illur";
+    
+    String department="BCA";
+    
+    String program="BCA";
+    
+    public String RegistertedCourse[]={" ", "C#", "Applied statistics", "Java"};
+    
+    public void display(){
+    
+    System.out.println("--Student Details----");
+    
+    System.out.println("Name:"+name);
+    
+    System.out.println("Department: "+department);
+    
+    System.out.println("Program: "+program);
+    
+    System.out.print("Registered Courses:\n");
+    
+    for(int i=1;i<=3;i++){
+    
+    System.out.println(i+":"+RegistertedCourse[i]);
+    
+    }
+    
+    }
+    
+    public static void main(String[] args){
+    
+    Student s = new Student();
+    
+    s.display();
+    
+    }
+    }
+    
+    
+    
+    
